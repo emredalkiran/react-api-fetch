aea534781e33e87ebc94cf3ba734eb96b67aa1fa
emredalkiran <emre59dalkiran@gmail.com>
2020-12-28 16:12:20 +0300

    React router added

--- src/index.js
+++ src/index.js
@@ -3,12 +3,15 @@ import ReactDOM from 'react-dom';
 import './main.css';
 import App from './App';
 import { Provider } from 'react-redux'
+import { BrowserRouter } from 'react-router-dom'
 import store from './store'
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
+      <BrowserRouter>
         <App />
+      </BrowserRouter>
     </Provider>
   </React.StrictMode>,
   document.getElementById('root')
